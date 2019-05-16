b93cdbc4e5889094d7cf6c9bede7f3332e6f803e
Ahmed Musa <ahmmus0101@gmail.com>
2019-05-16 13:07:10 +0200

    Update hangman2.java

--- hangman2.java
+++ hangman2.java
@@ -7,15 +7,15 @@ public class hangman2 {
 				//tar ett random ord
 	private static String word = ord[(int) (Math.random() * ord.length)];
 	//byter ut bokstäver mot *
-	private static String asterisk = new String(new char[word.length()]).replace("\0", "*");
+	private static String star = new String(new char[word.length()]).replace("\0", "*");
 	private static int count = 0;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		while (count < 7 && asterisk.contains("*")) { //while loop för att kunna skriva in gissningar
+		while (count < 7 && star.contains("*")) { //while loop för att kunna skriva in gissningar
 			System.out.println("Guess any letter in the word");
-			System.out.println(asterisk);
+			System.out.println(star);
 			String guess = sc.next();
 			hang(guess);
 		}
@@ -27,20 +27,20 @@ public class hangman2 {
 		for (int i = 0; i < word.length(); i++) { // en for loop som checkar om man har gissat rätt eller fel
 			if (word.charAt(i) == guess.charAt(0)) {
 				newasterisk += guess.charAt(0);
-			} else if (asterisk.charAt(i) != '*') {
+			} else if (star.charAt(i) != '*') {
 				newasterisk += word.charAt(i);
 			} else {
 				newasterisk += "*";
 			}
 		}
 
-		if (asterisk.equals(newasterisk)) {
+		if (star.equals(newasterisk)) {
 			count++;
 			hangmanbild();
 		} else {
-			asterisk = newasterisk;
+			star = newasterisk;
 		}
-		if (asterisk.equals(word)) { // om man har gissat alla bokstäver
+		if (star.equals(word)) { // om man har gissat alla bokstäver
 			System.out.println("Correct! You win! The word was " + word);
 		}
 	}
